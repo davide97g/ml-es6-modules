4be78e164db1c0123acec02c8952c80d39ca3880
davide97g <ghiotto.davidenko@gmail.com>
2019-07-02 17:23:07 +0200

    small fix and version update

--- src/js/nn/nn.js
+++ src/js/nn/nn.js
@@ -5,7 +5,7 @@ NeuralNet.prototype = {
     options = options || {};
     let layer_defs = options.layer_defs || [];
     if (layer_defs.length === 0) {
-      layer_defs.push({ type: "input", out_sx: 1, out_sy: 1, out_depth: 2 });
+      layer_defs.push({ type: "input", out_sx: 1, out_sy: 1, out_depth:2});
       layer_defs.push({ type: "fc", num_neurons: 4, activation: "tanh" });
       layer_defs.push({ type: "fc", num_neurons: 4, activation: "tanh" });
       layer_defs.push({ type: "fc", num_neurons: 4, activation: "tanh" });
@@ -23,28 +23,22 @@ NeuralNet.prototype = {
       l2_decay: training_options.l2_decay || 0.001
     });
 
-    let x = new Vol(1, 1, 2, 0.0);
 
     let maxiter = training_options.iters || 1000;
+    let zeroOneLabels = labels.map(label => (label === 1 ? 1 : 0));
     for (let iters = 0; iters < maxiter; iters++) {
       for (let i = 0; i < data.length; i++) {
-        x.w = data[i];
-        if (labels[i] === 1) trainer.train(x, 1);
-        else trainer.train(x, 0);
+        trainer.train(new Vol(data[i]), zeroOneLabels[i]);
       }
     }
   },
 
   predict: function(point) {
-    let input = new Vol(1, 1, 2, 0.0);
-    input.w = point;
-    let a = this.net.forward(input, false);
+    let a = this.net.forward(new Vol(point), false);
     return (Math.tanh(a.w[1] - a.w[0]) + 1) / 2;
   },
   predictClass: function(point) {
-    let input = new Vol(1, 1, 2, 0.0);
-    input.w = point;
-    let a = this.net.forward(input, false);
+    let a = this.net.forward(new Vol(point), false);
     return a.w[0] > a.w[1] ? -1 : 1;
   }
 };
